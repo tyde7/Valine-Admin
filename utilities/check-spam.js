4d7abc7111abba0457695e2c8be9c86cec8aaee7
Ding Shi <32335591+tyde7@users.noreply.github.com>
2020-02-21 02:08:32 +0800

    [bugfix] misspell

--- utilities/check-spam.js
+++ utilities/check-spam.js
@@ -33,7 +33,7 @@ exports.checkSpam = (comment, ip)=> {
             }, function(err, spam) {
                 if (err) console.log (`垃圾评论检测出错！${err}`);
                 if (spam) {
-                    console.log('逮到一只垃圾评论，烧死它！用文火~ 内容:${comment.get('comment')}');
+                    console.log(`逮到一只垃圾评论，烧死它！用文火~ 内容:${comment.get('comment')}`);
 //                     comment.set('isSpam', true);
 //                     comment.setACL(new AV.ACL({"*":{"read":false}}));
 //                     comment.save();
